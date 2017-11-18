cb7871f63e4555d601f168f562f504e7fceb455f
Jacoba <jacoba.brandner@web.de>
2017-11-18 15:31:44 +0100

    just one thing..

--- task2/steganohide.py
+++ task2/steganohide.py
@@ -1,7 +1,7 @@
 # needed for parameters
 import sys
 
-from utils import getBytesFromText, getPixels, setSecretImage, getSize
+from utils import *
 
 
 arguments = sys.argv
@@ -16,30 +16,67 @@ INPUT_IMAGE = arguments[1]
 # bild.bmp.ste
 OUTPUT = arguments[1] + ".ste.bmp"
 
+HEADER_OFFSET = 16
+BYTE_OFFSET = 8
+
+
 def storeContentInImage(pixel, content):
-	# we need enough space
-	# TODO: offset?
-	if len(content) > len(pixel)*3:
-		raise RuntimeError
+    # we need enough space
+    # TODO: offset?
+    if len(content) > len(pixel)*3:
+        raise RuntimeError
 
+    newPixel = pixel
 
+    # write the length header to the image
+    # QUESTION: 8 or 16 or 32?
+    #binaryContentLen = getBinary(len(content), 16)
+    #print binaryContentLen
+    #for x in range(0, len(binaryContentLen)):
+        #setLastBit(pixel, x, int(binaryContentLen[x]))
+
+    # iterate over all bits which should be written
+    for idx, value in enumerate(content):
+        # calculate the start position of the byte which should be written
+        # e.g. 16, 32, 40, 48, 56, ...
+        byteIndex = HEADER_OFFSET + BYTE_OFFSET * idx
+
+        # write every bit of the current byte
+        myBinary = getBinary(value, 8)
+
+        for x in range(0, len(myBinary)):
+            # strore each bit in Image
+            newValue = setLastBit(pixel, byteIndex + x, int(myBinary[x]))
+            newPixel[byteIndex + x] = newValue
+
+    return newPixel
 
 
 def main():
-	# get the content to write
-	content = getBytesFromText(arguments[0])
-
-	# get the pixels from image
-	# [(r,g,b)]
-	pixel = getPixels(INPUT_IMAGE)
-
-	# store the content in the pixels
-	storeContentInImage(pixel, content)
-
-	# write a new, modified image
-	setSecretImage(getSize(INPUT_IMAGE), pixel, OUTPUT)
+    createImage()
 
 
+def createImage():
+    # get the content to write
+    content = getBytesFromText(arguments[0])
 
-if __name__=="__main__":
-	main()
+    # get the pixels from image [(r,g,b)]
+    pixel = getPixels(INPUT_IMAGE)
+
+    # store the content in the pixels
+    newPixel = storeContentInImage(pixel, content)
+
+    # write a new, modified image
+    setSecretImage(getSize(INPUT_IMAGE), newPixel, OUTPUT)
+
+
+#def testImage():
+    # get the pixels from image [(r,g,b)]
+    #steImage = getPixels(OUTPUT)
+    # read the content in the pixels
+    #steContent = readContentInImage(steImage)
+    #write_string_to_file('text.ste.txt', steContent)
+
+
+if __name__ == "__main__":
+    main()
--- task2/utils.py
+++ task2/utils.py
@@ -1,32 +1,99 @@
 # pillow
 from PIL import Image
 
+
+# params: Value=Integer-byte, length=8 or 16
+def getBinary(value, length):
+    # bin(value) -> 0b110
+    # bin(value)[2:] -> 110
+    # zfill(8) -> 00000110
+    return bin(value)[2:].zfill(length)
+
+
 def getBytesFromText(filename):
-  bytes = []
+    bytes = []
 
-  # opening for [r]eading as [b]inary
-  with open(filename, "rb") as file:
-    while True:
-      byte = file.read(1)
-      # end of file
-      if not byte:
-        break
-      # convert the byte to an integer representation
-      bytes.append(ord(byte))
-  return bytes
+    # opening for [r]eading as [b]inary
+    with open(filename, "rb") as file:
+        while True:
+            byte = file.read(1)
+            # end of file
+            if not byte:
+                break
+            # convert the byte to an integer representation
+            bytes.append(ord(byte))
+    return bytes
 
+
+#
+def setLastBit(pixelArray, index, bitValue):
+    pixelIndex = int(index / 3)
+
+    # 0 1 2
+    listIndex = int(index % 3)
+
+    # [1,2,3] [225,1,43]
+    pixelList = list(pixelArray[pixelIndex])
+
+    # use (r) next (g) next (b)
+    # [33, 22, 77]  use 33
+    # [33, 44, 111] use 44
+    # [33, 255, 99] use 99 ...
+    currentValue = pixelList[listIndex]
+    #print currentValue
+    currentBinary = getBinary(currentValue, 8)
+
+    shorterBinary = currentBinary[:7]
+    lastBinary = currentBinary[7]
+
+    # print currentBinary
+    # print lastBinary
+    # print bitValue
+    # print shorterBinary
+
+    # TODO: set the last bit
+    if int(lastBinary) != bitValue:
+        mask = 1 << 8
+       # currentBinary = currentBinary | mask
+
+    print "tada " + str(currentBinary)
+
+    # convert Bits to Integer-Bytes
+    newValue = int(currentBinary, 2)
+
+    print newValue
+    # set the new value
+    pixelList[listIndex] = newValue
+
+    pixelArray[pixelIndex] = tuple(pixelList)
+
+    return pixelArray[pixelIndex * 3]
+
+
+#def readContentFromImage(pixelArray):
+
+
+# Returns the contents of an image as a
+# sequence object containing pixel values.
 def getPixels(imagename):
-  image = Image.open(imagename)
-  # Returns the contents of an image as a sequence object containing pixel values.
-  return list(image.getdata())
+    image = Image.open(imagename)
+    return list(image.getdata())
 
+
+# returns (width, height) of an image
 def getSize(imagename):
-  image = Image.open(imagename)
-  return image.size
+    image = Image.open(imagename)
+    return image.size
 
 
-# create new Image with secret message
+# create new Image with secret message :ghost:
 def setSecretImage(size, pixel, output):
-  new_image = Image.new('RGB', size)
-  new_image.putdata(pixel)
-  new_image.save(output)
+    new_image = Image.new('RGB', size)
+    new_image.putdata(pixel)
+    new_image.save(output)
+
+
+def write_string_to_file(filename, text):
+    f = open(filename, 'w')
+    f.write(text)
+    f.close()
