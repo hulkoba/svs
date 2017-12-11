9ac056dbcbe49d26ec992c22bd02194bb2143389
Hanna Prinz <hanna_prinz@yahoo.de>
2017-12-11 18:18:08 +0100

    XTEA cleaned up and with IV seed

--- task2/xtea.py
+++ task2/xtea.py
@@ -7,53 +7,35 @@ from operator import xor
 XTEA_DELTA = 0x9e3779b9L
 MASK = 0xffffffffL
 
+
 def xor_block_enc(password, iv, content, rounds):
     enc = encipher(password, content, rounds)
-
-    print("ENC " + str(password) + " | " + str(iv) + " | " + str(content) + " = " + str(enc))
+    print("ENC " + str(password) + " | iv = " + str(iv) + " | content = " + str(content) + " | enc = " + str(enc))
 
     result = sxor(enc, iv)
     print("    result = " + str(result))
+
     return result
 
 
 def xor_block_dec(password, iv, content, rounds):
     unxor = sxor(content, iv)
-
-    print("DEC " + str(password) + " | " + str(iv) + " | " + str(content) + " = " + str(unxor))
+    print("DEC " + str(password) + " | iv = " + str(iv) + " | content = " + str(content) + " | dec = " + str(unxor))
 
     result = decipher(password, unxor, rounds)
-
     print("    result = " + str(result))
-    return result
-
-    '''
-    print("enc + " + str(type(enc)))
-
-    binary_iv = ""
-    for c in iv:
-        binary_iv += getBinary(ord(c))
-
-    binary_enc = ""
-    for c in enc:
-        binary_enc += getBinary(ord(c))
-
-    result = ""
-    for x,y in zip(binary_iv, binary_enc):
-        xor_v = xor(int(x), int(y))
-        result += str(xor_v)
 
     return result
-    '''
 
 
-# def crypt(key,data,iv='\00\00\00\00\00\00\00\00',n=32):
 def crypt(password, content, iv, rounds=32, mode='CFB', enc=True):
     '''
     :param password: hashed password of length 12
     :param content: content to be encrypted
     :param iv: generated seed for the key generator of length 8
     :param rounds: number of rounds (defaults to 32)
+    :param mode: XTEA-Mode (CFB, ...)
+    :param enc: encode or decode
     :return: the encrypted text
     '''
 
@@ -62,26 +44,21 @@ def crypt(password, content, iv, rounds=32, mode='CFB', enc=True):
 
     # todo switch over mode
 
-    #content = ''.join(x.encode('hex') for x in content)
-
-    #ord_content = map(ord, content)
-
-    # print("content = " + str(content))
-    # print("ord_content = " + str(ord_content))
+    # todo feed new iv to next round?
 
     result = []
     for i in range(len(content) / 8):
         block = content[i * 8:i * 8 + 8]
         if enc:
-            encoded = xor_block_enc(password, iv, block, rounds)
-            result.append(encoded)
+            print("iv =" + str(iv))
+            iv = xor_block_enc(password, iv, block, rounds)
+            result.append(iv)
         else:
-            decoded = xor_block_dec(password, iv, block, rounds)
-            result.append(decoded)
+            print("iv =" + str(iv))
 
-        #print("key = " + str(iv))
+            result.append(xor_block_dec(password, iv, block, rounds))
+            iv = block
 
-    #res = ''.join(x.encode('hex') for x in result)
     return "".join(result)
 
 
@@ -90,6 +67,7 @@ def encipher(password, block, num_rounds):
     k = struct.unpack("!4L", password)
 
     sum_value = 0L
+
     for r in range(num_rounds):
         v0 = (v0 + (((v1 << 4 ^ v1 >> 5) + v1) ^ (sum_value + k[sum_value & 3]))) & MASK
         sum_value = (sum_value + XTEA_DELTA) & MASK
@@ -100,15 +78,11 @@ def encipher(password, block, num_rounds):
 def decipher(password, block, num_rounds):
     v0, v1 = struct.unpack("!2L", block)
     k = struct.unpack("!4L", password)
+
     sum_value = (XTEA_DELTA * num_rounds) & MASK
+
     for r in range(num_rounds):
         v1 = (v1 - (((v0 << 4 ^ v0 >> 5) + v0) ^ (sum_value + k[sum_value >> 11 & 3]))) & MASK
         sum_value = (sum_value - XTEA_DELTA) & MASK
         v0 = (v0 - (((v1 << 4 ^ v1 >> 5) + v1) ^ (sum_value + k[sum_value & 3]))) & MASK
     return struct.pack("!2L", v0, v1)
-
-# encrypted = crypt("0123456789012345", "kljshfdls kjfskksf lash dk gse hliuewi7t5 437tl   Text", "ABCDEFGH")
-# print("enc = " + str(encrypted))
-
-# decrypted = crypt("0123456789012345", encrypted, "ABCDEFGH")
-# print("dec = " + str(decrypted))
