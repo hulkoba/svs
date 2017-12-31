62d40be90765c9909a8574b14aacf4339b102b8b
Hanna Prinz <hanna_prinz@yahoo.de>
2017-12-31 20:32:11 +0100

    script

---
+++ task3/key_exchange.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo
+echo "======================================================="
+echo "Alice sends Bob the message \"Live long and prosper\""
+echo
+python2 native_dh.py -s -k pass -u "alice@mail.org" -m bob@mail.org "Live long and prosper"
+
+echo
+echo "======================================================="
+echo "Bob checking his mails"
+echo
+python2 native_dh.py -r -k pass -u "bob@mail.org"
+
+echo
+echo "======================================================="
+echo "Alice checking her mails"
+echo
+python2 native_dh.py -r -k pass -u "alice@mail.org"
+
+echo
+echo "======================================================="
+echo "Alice sends Bob the message \"Live long and prosper\""
+echo
+python2 native_dh.py -s -k pass -u "alice@mail.org" -m bob@mail.org "Live long and prosper"
